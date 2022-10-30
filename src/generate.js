5d2a824f66d15500d8cd37ecf19c9978aeb83979
pikpokjeon <pikpokjeon@gmail.com>
2022-10-30 12:25:02 +0900

    FEAT 쿠브 랜덤으로 섞는 함수

--- src/generate.js
+++ src/generate.js
@@ -19,28 +19,42 @@ const genCubeData = ( {type, num, idx, key} ) =>
 export const genNumArr = ( count ) => Array( count ).fill( 1 ).map( ( _, i ) => _ + i )
 
 // 14, 2, types, jockerCount 2
-export const genCubes = () =>
+export const genCubesByTypes = () =>
 {
-    let idx = -1
-    const cubesByType = CUBE.TYPES.reduce( ( acc, type ) =>
+    let idx = 0
+    return CUBE.TYPES.reduce( ( acc, type ) =>
     {
         const arr = type === 'j' ? genNumArr( CUBE.JOCKER_SET ) : genNumArr( CUBE.TYPE_LEN )
         const cubeDataArr = arr.map( ( num ) =>
-        {
-            const cubeDataByKey = genNumArr( CUBE.TYPE_KEY_COUNT ).map( ( _, key ) =>
-            {
-                idx++
-                return genCubeData( {type, num, idx, key} )
-
-
-            } )
-            return cubeDataByKey
-        }
-        ).flat( 1 )
-        acc = {...acc, [type]: cubeDataArr}
-        return acc
+            genNumArr( CUBE.TYPE_KEY_COUNT ).map( ( _, key ) =>
+                genCubeData( {type, num, idx: idx++, key} ) ) ).flat( 1 )
+        return {...acc, [type]: cubeDataArr}
     }, {} )
 
-    console.log( cubesByType )
 }
 
+const genRandomInRange = ( min, max ) => Math.floor( Math.random() * max + min )
+
+export const mixCubes = cubes =>
+{
+    const _cubes = [...cubes]
+    return cubes.reduce( ( box, _, i ) =>
+    {
+        while ( 1 )
+        {
+            const idx = genRandomInRange( 0, _cubes.length )
+            if ( _cubes[idx] )
+            {
+                box[i] = _cubes[idx], delete ( _cubes[idx] )
+                break
+            }
+            continue
+        }
+        return box
+    }, [] )
+}
+
+export const mixCubesByType = ( cubesByType = {} ) =>
+    Object.entries( cubesByType )
+        .reduce( ( acc, [type, cubes] ) =>
+            ( {...acc, [type]: mixCubes( cubes )} ), {} )
